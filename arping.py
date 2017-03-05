a783ab6019e05f2ccdccad8b1fad40fb38a5c9c8
Reversaidx <fedomarazm@gmail.com>
2017-03-05 20:45:28 +0800

    norm

--- arping.py
+++ arping.py
@@ -1,6 +1,16 @@
-import os,subprocess
+import os,subprocess,re
 from glob import glob
 ifcfglist=[]
-os.chdir('/etc/sysconfig/network-scripts')
-ifcfglist=glob("ifcfg-eth*")
-print (ifcfglist)
+iplist=[]
+ifcfglist=glob("/etc/sysconfig/network-scripts/ifcfg-eth*")
+ip=re.compile(r'.*IPADDR0=(?P<ip>\S+).*')
+
+for i in ifcfglist:
+    file=open(i,"r")
+    str=file.read()
+    iplist.append(ip.findall(str))
+
+for i in iplist:
+    if i:
+        subprocess.call('arping -c 1 -I eth0 -s %d 192.168.0.1' %int(i), shell=True)
+
