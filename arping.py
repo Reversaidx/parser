2eb58e879f9dc5488dbdde54c798f4e6b3050600
Reversaidx <fedomarazm@gmail.com>
2017-03-05 21:00:27 +0800

    teper tochno norm

--- arping.py
+++ arping.py
@@ -12,5 +12,5 @@ for i in ifcfglist:
 
 for i in iplist:
     if i:
-        subprocess.call('arping -c 1 -I eth0 -s %d 192.168.0.1' %int(i), shell=True)
+        subprocess.call('arping -c 1 -I eth0 -s %s 192.168.0.1' %i[0], shell=True)
 
